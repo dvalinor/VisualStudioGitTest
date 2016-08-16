abd7c99459e6adb3f6688c3daa0a96732dc7208f
Rasmus Bjerre <dvalinor@gmail.com>
2016-08-16 10:47:34 +0200

    Initiated project

---
+++ GitVisualStudioTest/GitVisualStudioTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitVisualStudioTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+        }
+    }
+}
